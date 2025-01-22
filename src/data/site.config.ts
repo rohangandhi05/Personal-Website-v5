68b0a55c69e7738309c1679273dc4f264361d200
Rohan <gandhirohan05@gmail.com>
2025-01-21 22:57:49 -0800

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,10 +9,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'Mike Hoang',
-	title: 'Mike Hoang - Software Developer',
+	author: 'Rohan Gandhi',
+	title: 'Rohan Gandhi - Software Developer',
 	description:
-		'Mike Hoang is a software developer with a passion for creating innovative web applications, who always strive to make the complex seem effortless.',
+		'Rohan Gandhi is a software developer with a passion for creating innovative web applications, who always strive to make the complex seem effortless.',
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
